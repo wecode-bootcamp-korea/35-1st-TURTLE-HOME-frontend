3d563f708635048ad030fc4b8177569864562712
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-07-19 16:59:33 +0900

    Add: Search Page 레이아웃 완료

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import './Search.scss';
 
 const Search = () => {
-  return <div></div>;
+  return (
+    <main>
+      <section className="search-bar">
+        <form>
+          <input type="text" placeholder="무엇을 찾으세요?"></input>
+        </form>
+        <div className="x-box">
+          <img src="/images/x_thin.png" alt="Exit from the search page" />
+        </div>
+      </section>
+      <section className="products-list">
+        <div className="result">검색어를 입력해 주세요</div>
+      </section>
+    </main>
+  );
 };
 
 export default Search;
