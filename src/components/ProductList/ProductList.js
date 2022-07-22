98845621e253abacfd0abe982e3a85383546133d
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-07-22 16:44:06 +0900

    Branch 옮겨 갔다 옵니다..

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ProductList.scss';
 
 const ProductList = ({ url, alt, name, price }) => {
   return (
     <li className="product-item">
-      <div className="product-img">
-        <img src={url} alt={alt} />
-      </div>
+      <Link to="">
+        <div className="product-img">
+          <img src={url} alt={alt} />
+        </div>
+      </Link>
       <div className="product-info">
         <h2>{name}</h2>
         <div className="product-price">
