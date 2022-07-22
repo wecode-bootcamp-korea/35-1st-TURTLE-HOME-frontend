b2cb3e98cd23c0fb7914a212b9bd915749ee181c
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-07-22 20:17:30 +0900

    Add: 가격 표출 수정

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,15 +3,30 @@ import { Link } from 'react-router-dom';
 import './Product.scss';
 
 const Product = ({ url, alt, name, price }) => {
+  const handlePrice = () => {
+    if (price.length === 1) {
+      return <div>{price[0].toLocaleString('ko-KR')} 원</div>;
+    } else {
+      return (
+        <div>
+          {price[0].toLocaleString('ko-KR')} 원 -{' '}
+          {price[price.length - 1].toLocaleString('ko-KR')} 원
+        </div>
+      );
+    }
+  };
+
   return (
-    <li className="product">
-      <Link to="/">
-        <img src={url} alt={alt} />
-        <div className="product-info">
-          <h2>{name}</h2>
-          <p>{price.toLocaleString('ko-KR')}원</p>
+    <li className="search-product-item">
+      <Link to="">
+        <div className="product-img">
+          <img src={url} alt={alt} />
         </div>
       </Link>
+      <div className="product-info">
+        <h2>{name}</h2>
+        {handlePrice()}
+      </div>
     </li>
   );
 };
