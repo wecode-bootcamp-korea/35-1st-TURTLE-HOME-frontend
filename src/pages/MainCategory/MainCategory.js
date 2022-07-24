d3f58adf16cd258698af984ddfe7b8c3b33f3918
1eo21eoe <1eo21eoe@gmail.com>
2022-07-24 22:44:15 +0900

    Add: 메인 카테고리 네비게이트 연결

--- src/pages/MainCategory/MainCategory.js
+++ src/pages/MainCategory/MainCategory.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './MainCategory.scss';
 
 const MainCategory = () => {
   const [category, setCategory] = useState([]);
   const number = 1;
-
+  const navigate = useNavigate();
+  const goToSubCategory = () => {
+    navigate(`category/${category.id}`);
+  };
+  console.log(category);
   useEffect(() => {
     fetch(`http://10.58.7.243:8000/categories/${number}/subcategories`)
       .then(res => res.json())
@@ -16,7 +21,7 @@ const MainCategory = () => {
   return (
     <div className="category_box">
       {category.map(list => (
-        <div className="category_list" key={list.id}>
+        <div className="category_list" key={list.id} onClick={goToSubCategory}>
           <img src={list.image_url} alt="category list" />
           <p>{list.name}</p>
         </div>
