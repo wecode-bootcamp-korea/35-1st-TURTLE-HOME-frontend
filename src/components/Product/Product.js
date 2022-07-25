fa03bcfb7ac535e2886ec63f33b524d7c6497a4f
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-07-25 17:23:21 +0900

    Modify: props 수정

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Product.scss';
 
-const Product = ({ id, url, alt, name, price }) => {
+const Product = ({ id, url, name, prices }) => {
   const navigate = useNavigate();
   const goToProductDetail = () => {
     navigate(`/products/${id}`);
@@ -11,16 +11,16 @@ const Product = ({ id, url, alt, name, price }) => {
   return (
     <li className="search-product-item">
       <div className="product-img" onClick={goToProductDetail}>
-        <img src={url} alt={alt} />
+        <img src={url} alt={name} />
       </div>
       <div className="product-info">
         <h2>{name}</h2>
-        {price.length === 1 ? (
-          <div>{price[0].toLocaleString('ko-KR')} 원</div>
+        {prices.length === 1 ? (
+          <div>{prices[0].toLocaleString('ko-KR')} 원</div>
         ) : (
           <div>
-            {price[0].toLocaleString('ko-KR')} 원 -{' '}
-            {price[price.length - 1].toLocaleString('ko-KR')} 원
+            {prices[0].toLocaleString('ko-KR')} 원 -{' '}
+            {prices[prices.length - 1].toLocaleString('ko-KR')} 원
           </div>
         )}
       </div>
