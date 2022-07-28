1917f4a87e5656de520f8e2f05eb129f9ab64bbc
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-07-28 13:39:22 +0900

    Conflict 해결

--- src/Router.js
+++ src/Router.js
@@ -4,8 +4,6 @@ import Header from './components/Header/Header';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Nav from './components/Nav/Nav';
 import Intro from './pages/Intro/Intro';
-import SignUp from './pages/SignUp/SignUp';
-import Login from './pages/Login/Login';
 import Search from './pages/Search/Search';
 import MainCategory from './pages/MainCategory/MainCategory';
 import SubCategory from './pages/SubCategory/SubCategory';
@@ -19,8 +17,6 @@ const Router = () => {
       <Nav />
       <Routes>
         <Route path="/" element={<Intro />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/search" element={<Search />} />
         <Route path="/maincategory" element={<MainCategory />} />
         <Route path="/products" element={<SubCategory />} />
